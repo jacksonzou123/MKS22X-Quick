5e54772899e6400667166645d8d6bad1c7c77280
Jackson Zou <jzou00@stuy.edu>
2019-03-14 23:51:50 -0400

    working quickSort

--- Quick.java
+++ Quick.java
@@ -22,16 +22,17 @@ public class Quick{
      return data[current];
    }
 
-   public static void quickSort(int[] ary) {
+   public static void quicksort(int[] ary) {
      quickSortH(ary, 0, ary.length-1);
    }
 
    public static void quickSortH(int[] ary, int start, int end) {
-     if (end - start > 0) {
-       int current = partition(ary, start, end);
-       quickSortH(ary, 0, current - 1);
-       quickSortH(ary, current + 1, end);
+     if (start >= end) {
+       return;
      }
+     int current = partition(ary, start, end);
+     quickSortH(ary, start, current - 1);
+     quickSortH(ary, current + 1, end);
    }
 
 
@@ -48,7 +49,8 @@ public class Quick{
        return ary[start];
      }
      Random random = new Random();
-     int rand = Math.abs(random.nextInt()) % (end - start);
+     int rand = Math.abs(random.nextInt()) % (end - start) + start;
+
      //System.out.println(rand);
      int target = ary[rand];
      //System.out.println("rand: " + rand + "target: "  + target);
@@ -56,7 +58,6 @@ public class Quick{
      ary[start] = target;
      int begin = start;
      start++;
-     boolean back = true;
      while (start < end) {
        if (target > ary[start]) {
          start++;
@@ -68,22 +69,21 @@ public class Quick{
          end--;
        }
        if (target == ary[start]) {
-         if (back) {
+         int num = Math.abs(random.nextInt()) % 2;
+         if (num == 0) {
            int swap = ary[end];
            ary[end] = ary[start];
            ary[start] = swap;
            end--;
-           back = false;
          }
          else {
            start++;
-           back = true;
          }
        }
      }
      //System.out.println(start);
      //System.out.println(Arrays.toString(ary));
-     if (target > ary[start]) {
+     if (target >= ary[start]) {
        ary[begin] = ary[start];
        ary[start] = target;
        return start;
@@ -96,7 +96,7 @@ public class Quick{
      return 0;
    }
 
-   public static void main(String[] args) {
+   /*public static void main(String[] args) {
      Random random = new Random();
      int[] ary = new int[Integer.parseInt(args[0])];
      for (int i = 0; i < Integer.parseInt(args[0]); i++) {
@@ -106,4 +106,41 @@ public class Quick{
      System.out.println("index: " + f);
      System.out.println(Arrays.toString(ary));
    }
+   */
+   public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Quick.quicksort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
+  }
+}
+
+
 }
